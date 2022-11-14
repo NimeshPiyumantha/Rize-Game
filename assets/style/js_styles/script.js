c01365f675ae8e6a85728c47ba1505230eb8a09c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-11-14 20:44:40 +0530

    Add script to index1 ideal image

--- assets/style/js_styles/script.js
+++ assets/style/js_styles/script.js
@@ -20,3 +20,23 @@ audio4.src = "assets/audio/GameOver.mp3";
 let audio5 = new Audio();
 audio5.src = "assets/audio/ActCleared.mp3";
 
+
+let boy = document.getElementById("boy");
+let idleAnimationNumber = 0;
+let idleImageNumber = 1;
+
+//Idle Animation
+function idleAnimation() {
+    idleImageNumber = idleImageNumber + 1;
+
+    if (idleImageNumber === 11) {
+        idleImageNumber = 1;
+    }
+
+    boy.src = "assets/img/png/idle (" + idleImageNumber + ").png";
+}
+
+function idleAnimationStart() {
+    idleAnimationNumber = setInterval(idleAnimation, 200);
+}
+
