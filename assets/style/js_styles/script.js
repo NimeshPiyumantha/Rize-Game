2393922d6642c48b0fc0753159a38171fb65241e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-11-14 20:44:02 +0530

    Add script to index1 audio

---
+++ assets/style/js_styles/script.js
@@ -0,0 +1,22 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+let audio1 = new Audio();
+audio1.src = "assets/audio/BridgeZone.mp3";
+audio1.play();
+audio1.loop = true;
+
+let audio2 = new Audio();
+audio2.src = "assets/audio/RingCollect.mp3";
+
+let audio3 = new Audio();
+audio3.src = "assets/audio/GameOver2.wav";
+
+let audio4 = new Audio();
+audio4.src = "assets/audio/GameOver.mp3";
+
+let audio5 = new Audio();
+audio5.src = "assets/audio/ActCleared.mp3";
+
