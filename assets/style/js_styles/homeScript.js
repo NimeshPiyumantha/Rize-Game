50cd9cfd548fde52368bfd60f2cbcf8abe29998d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-11-14 06:21:20 +0530

    Add sound controller button js

--- assets/style/js_styles/homeScript.js
+++ assets/style/js_styles/homeScript.js
@@ -6,3 +6,25 @@ let audio1 = new Audio();
 audio1.src = "assets/audio/TitleScreen.mp3";
 audio1.play();
 
+(function () {
+
+    if (!audio1.paused) {
+        $("div#controller-icons>button#sound-icon").addClass("pressed");
+    } else {
+        $("div#controller-icons>button#sound-icon").removeClass("pressed");
+    }
+
+})();
+
+
+
+$("#sound-icon").click(function (e) {
+    if (!audio1.paused) {
+        audio1.pause();
+        $("div#controller-icons>button#sound-icon").removeClass("pressed");
+    } else {
+        audio1.play();
+        $("div#controller-icons>button#sound-icon").addClass("pressed");
+    }
+});
+
