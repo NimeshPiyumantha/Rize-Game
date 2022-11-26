5e8760b097954d3eb3f38ee7bbabf2b011e154b5
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-11-26 11:35:10 +0530

    Refactor content

--- assets/style/js_styles/level_One_Script.js
+++ assets/style/js_styles/level_One_Script.js
@@ -33,7 +33,7 @@ function idleAnimation() {
         idleImageNumber = 1;
     }
 
-    boy.src = "../../assets/img/png/idle ("+idleImageNumber+").png";
+    boy.src = "assets/img/png/idle ("+idleImageNumber+").png";
 
 }
 
